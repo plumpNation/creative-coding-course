852086ab5a5a1320065497a138a0579e195fac19
Gavin King <gavin.king@cgi.com>
2022-11-28 13:06:14 +0100

    Drawing rectangle with translated origin

--- sketch.js
+++ sketch.js
@@ -7,9 +7,35 @@ const settings = {
 };
 
 const sketch = () => {
+  let x, y, w, h;
+
   return ({ context, width, height }) => {
-    context.fillStyle = 'red';
-    context.fillRect(0, 0, width, height);
+    // context.fillStyle = 'blue';
+    // context.fillRect(0, 0, width, height);
+
+    w = width * 0.6;
+    h = width * 0.1;
+    x = width * 0.5;
+    y = width * 0.5;
+
+    context.save()
+    context.translate(x, y); // center
+    context.translate(w * -0.5, h * -0.5); // move draw origin
+
+    context.strokeStyle = 'blue';
+
+    // context.strokeRect(0, 0, w, h);
+
+    context.beginPath();
+    context.moveTo(0, 0);
+    context.lineTo(w, 0);
+    context.lineTo(w, h);
+    context.lineTo(0, h);
+    context.lineTo(0, 0);
+
+    context.stroke();
+
+    context.restore()
   };
 };
 
