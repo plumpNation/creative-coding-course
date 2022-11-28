546154af6ac68305680cdf4250b03aa45e4ae8ec
Gavin King <gavin.king@cgi.com>
2022-11-28 15:51:50 +0100

    Popped shadows and blend effects on my rectangles

--- sketch.js
+++ sketch.js
@@ -1,5 +1,8 @@
 const canvasSketch = require('canvas-sketch');
-const math = require('canvas-sketch-util/math');
+const mathUtils = require('canvas-sketch-util/math');
+const randomUtils = require('canvas-sketch-util/random');
+const colorUtils = require('canvas-sketch-util/color');
+const risoColors = require('riso-colors');
 
 const settings = {
   dimensions: [ 800, 800 ],
@@ -15,7 +18,7 @@ const settings = {
  * @returns {{x: number, y: number}}
  */
 const getCartesianCoords = (angle, radius) => {
-  const radians =  math.degToRad(angle)
+  const radians =  mathUtils.degToRad(angle)
 
   return {
     x: Math.cos(radians) * radius,
@@ -26,57 +29,124 @@ const getCartesianCoords = (angle, radius) => {
 const drawSkewedRectangle = ({
   context,
   degrees,
-  width = 600,
-  height = 200,
+  w = 600,
+  h = 200
 }) => {
-  const { x, y } = getCartesianCoords(degrees, width);
+  const { x, y } = getCartesianCoords(degrees, w);
 
-  context.translate(x * -0.5, (y + height) * -0.5); // move draw origin
+  context.translate(x * -0.5, (y + h) * -0.5); // move draw origin
 
   context.save();
 
   context.beginPath();
   context.moveTo(0, 0);
   context.lineTo(x, y);
-  context.lineTo(x, y + height);
-  context.lineTo(0, height);
+  context.lineTo(x, y + h);
+  context.lineTo(0, h);
   context.lineTo(0, 0);
-
-  context.stroke();
 
   context.restore();
 }
 
-const sketch = () => {
-  let x, y, w, h;
+const deriveLesserColor = (color, luminance, opacity) => {
+  const result = colorUtils.offsetHSL(color, 0, 0, luminance);
 
+  result.rgba[3] = opacity;
+
+  return result.rgba
+}
+
+const configureShadow = ({
+  context,
+  color = 'rgba(0, 0, 0, 1)',
+  offsetX = 0,
+  offsetY = 0,
+  blur = 0
+}) => {
+  context.shadowColor = color;
+  context.shadowOffsetX = offsetX;
+  context.shadowOffsetY = offsetY;
+  context.shadowBlur = blur
+}
+
+const sketch = ({ width, height }) => {
+  const repeats = 40;
+  const degrees = -30;
+
+  const rects = [];
+
+  const rectColors = [
+    randomUtils.pick(risoColors).hex,
+    randomUtils.pick(risoColors).hex
+  ]
+
+  const bgColor = randomUtils.pick(risoColors).hex;
+
+  // Since the render function is called when
+  // saving an image from the browser, we need
+  // to create collections outside the render
+  // so that the same collection is available
+  // to each render tick.
+  // Save, refresh, will of course run this code again
+  // and therefore generate a new collection.
+  for (let i = 0; i < repeats; i += 1) {
+    const fill = randomUtils.pick(rectColors);
+    const stroke = randomUtils.pick(rectColors);
+
+    const x = randomUtils.range(0, width);
+    const y = randomUtils.range(0, height);
+    const w = randomUtils.range(600, width);
+    const h = randomUtils.range(40, 200);
+
+    rects.push({ x, y, w, h, fill, stroke });
+  }
+
+  /**
+   * This is the render function
+   *
+   * @param {{ context: CanvasRenderingContext2D, width: number, height: number }} opts
+   */
   return ({ context, width, height }) => {
-    // calculations
-    // ///////////////////////////
+    context.fillStyle = bgColor;
+    context.fillRect(0, 0, width, height);
 
-    w = width * 0.6;
-    h = width * 0.1;
-    x = width * 0.5;
-    y = width * 0.5;
+    rects.forEach(({ x, y, w, h, fill, stroke }) => {
+      context.save()
+      context.translate(x, y); // center
+      context.strokeStyle = stroke;
+      context.fillStyle = fill;
+      context.lineWidth = 10;
 
-    // each clock segment angle is 30 degrees
-    // 0 degrees is 3 o clock
-    // 30 degrees is 4 o clock etc
-    const degrees = 360 / 12;
+      context.globalCompositeOperation = randomUtils.value() > 0.5 ? 'overlay' : 'source-over';
 
-    // drawing
-    // ///////////////////////////
+      drawSkewedRectangle({ context, w, h, degrees })
 
-    context.save()
-    context.translate(x, y); // center
-    context.strokeStyle = 'blue';
+      // We don't want shadow on the stroke, only the fill
+      // so we want to save a snapshot of the context to restore.
+      context.save();
 
-    // context.strokeRect(0, 0, w, h);
+      configureShadow({
+        context,
+        color: colorUtils.style(deriveLesserColor(fill, -20, 0.8)),
+        offsetX: -10,
+        offsetY: 20,
+        blur: 8,
+      })
 
-    drawSkewedRectangle({ context, degrees })
-    context.stroke();
+      context.fill();
+      // restore to reset the shadowColor
+      context.restore();
 
-    context.restore();
+      context.stroke();
+
+      context.globalCompositeOperation = 'source-over';
+
+      context.lineWidth = 1;
+      context.strokeStyle = 'black';
+      context.stroke();
+
+      context.restore();
+    })
   };
 };
 
