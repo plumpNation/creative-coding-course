2e43c5d6815590c13a4bea0d25022954221dfa61
Gavin King <gavin.king@cgi.com>
2022-11-28 23:31:39 +0100

    Doing some cleanup to make it easier to edit things later

--- sketch.js
+++ sketch.js
@@ -4,14 +4,17 @@ const randomUtils = require('canvas-sketch-util/random');
 const colorUtils = require('canvas-sketch-util/color');
 const risoColors = require('riso-colors');
 
+const artboard = 1000
+
 const settings = {
-  dimensions: [ 1080, 1080 ],
+  dimensions: [ artboard, artboard ],
   // animate: true,
   fps: 60,
 };
 
 /**
- * Calculate the coordinates required for a specific angle.
+ * Calculate the coordinates for a specific angle
+ * at a specific radius from starting point.
  *
  * @param {number} angle
  * @param {number} radius
@@ -26,8 +29,11 @@ const getCartesianCoords = (angle, radius) => {
   }
 }
 
-const drawSkewedRectangle = ({
-  context,
+/**
+ * @param {CanvasRenderingContext2D} context
+ * @param {{ degrees: number; w: number; h: number; }} options
+ */
+const drawSkewedRectangle = (context, {
   degrees,
   w = 600,
   h = 200
@@ -44,7 +50,11 @@ const drawSkewedRectangle = ({
   context.closePath();
 }
 
-const drawPolygon = ({ context, radius = 300, sides = 6 }) => {
+/**
+ * @param {CanvasRenderingContext2D} context
+ * @param {{ radius: number, sides: number }} options
+ */
+const drawPolygon = (context, { radius = 300, sides = 6 }) => {
   const slice = Math.PI * 2 / sides;
 
   context.beginPath();
@@ -77,19 +87,33 @@ const deriveLesserColor = (color, luminance, opacity) => {
   return result.rgba
 }
 
+/**
+ * @param {CanvasRenderingContext2D} context
+ * @param {number} width
+ * @param {number} height
+ */
 const centerTranslation = (context, width, height) => {
   // 0.58 is a hack to centre the triangle in the easiest way
   context.translate(width * 0.5, height * 0.58);
 }
 
+/**
+ * Safe and predictable way to enforce context is always restored.
+ *
+ * @param {CanvasRenderingContext2D} context
+ * @param {() => {}} cb
+ */
 const subContext = (context, cb) => {
   context.save();
   cb();
   context.restore();
-}
+};
 
-const configureShadow = ({
-  context,
+/**
+ * @param {CanvasRenderingContext2D} context
+ * @param {{ color: string; offsetX: number; offsetY: number; blur: number }} options
+ */
+const configureShadow = (context, {
   color = 'rgba(0, 0, 0, 1)',
   offsetX = 0,
   offsetY = 0,
@@ -99,8 +123,27 @@ const configureShadow = ({
   context.shadowOffsetX = offsetX;
   context.shadowOffsetY = offsetY;
   context.shadowBlur = blur
+};
+
+/**
+ * Create a mask, and add contents.
+ * @param {CanvasRenderingContext2D} context
+ * @param {() => void} createMask
+ * @param {() => void} createContents
+ */
+const clip = (context, createMask, createContents) => {
+  createMask();
+  context.clip();
+  createContents();
+  context.restore(); // needed after clip
 }
 
+/**
+ * This is the main application controller.
+ * It is run by the canvas-sketch application.
+ *
+ * @param {{ context: CanvasRenderingContext2D, width: number, height: number }} options
+ */
 const sketch = ({ width, height }) => {
   const repeats = 40;
   const degrees = -30;
@@ -115,7 +158,7 @@ const sketch = ({ width, height }) => {
   const bgColor = randomUtils.pick(risoColors).hex;
   const outerPolyStrokeColor = randomUtils.pick(rectColors);
 
-  const mask = {
+  const maskOptions = {
     radius: width * 0.3,
     sides: 3,
   };
@@ -133,8 +176,13 @@ const sketch = ({ width, height }) => {
 
     const x = randomUtils.range(0, width);
     const y = randomUtils.range(0, height);
-    const w = randomUtils.range(600, width);
-    const h = randomUtils.range(40, 200);
+    const w = randomUtils.range(width - (width * 0.3), width);
+
+    const rectMaxHeight = height * 0.2;
+
+    const h = randomUtils.range(rectMaxHeight - (rectMaxHeight * 0.9), rectMaxHeight);
+
+    console.log(h)
 
     rects.push({ x, y, w, h, fill, stroke });
   }
@@ -142,75 +190,82 @@ const sketch = ({ width, height }) => {
   /**
    * This is the render function
    *
-   * @param {{ context: CanvasRenderingContext2D, width: number, height: number }} opts
+   * @param {{ context: CanvasRenderingContext2D, width: number, height: number }} options
    */
   return ({ context, width, height }) => {
     context.fillStyle = bgColor;
     context.fillRect(0, 0, width, height);
 
-    subContext(context, () => {
-      centerTranslation(context, width, height);
-
-      drawPolygon({ context, ...mask });
-    });
-
-    context.clip();
-
-    rects.forEach(({ x, y, w, h, fill, stroke }) => {
+    clip(context, () => {
+      // mask
       subContext(context, () => {
-        context.translate(x, y); // center
-        context.strokeStyle = stroke;
-        context.fillStyle = fill;
-        context.lineWidth = 10;
+        centerTranslation(context, width, height);
 
-        context.globalCompositeOperation = randomUtils.value() > 0.5 ? 'overlay' : 'source-over';
-
-        drawSkewedRectangle({ context, w, h, degrees })
-
+        drawPolygon(context, maskOptions);
+      });
+    }, () => {
+      // mask contents
+      rects.forEach(({ x, y, w, h, fill, stroke }) => {
         subContext(context, () => {
-          // We don't want shadow on the stroke, only the fill
-          configureShadow({
-            context,
-            color: colorUtils.style(deriveLesserColor(fill, -20, 0.8)),
-            offsetX: -10,
-            offsetY: 20,
-            blur: 8,
+          // DRAW A RECTANGLE
+          context.translate(x, y);
+          context.strokeStyle = stroke;
+          context.fillStyle = fill;
+          context.lineWidth = artboard * 0.01;
+
+          // Randomly use overlay blend mode on rectangle.
+          context.globalCompositeOperation = randomUtils.value() > 0.5 ? 'overlay' : 'source-over';
+
+          drawSkewedRectangle(context, { w, h, degrees })
+
+          subContext(context, () => {
+            // ADD SHADOW TO THE FILL
+            configureShadow(context, {
+              color: colorUtils.style(deriveLesserColor(fill, -20, 0.8)),
+              offsetX: -10,
+              offsetY: 20,
+              blur: 8,
+            });
+
+            context.fill();
           });
 
-          context.fill();
+          context.stroke();
+
+          // We don't want the next stroke to be blend mode
+          context.globalCompositeOperation = 'source-over';
+
+          context.lineWidth = artboard * 0.001;
+          context.strokeStyle = 'black';
+          context.stroke();
         });
-
-        context.stroke();
-
-        context.globalCompositeOperation = 'source-over';
-
-        context.lineWidth = 1;
-        context.strokeStyle = 'black';
-        context.stroke();
       });
     });
 
-    context.restore(); // need one to stop clipping
-
     subContext(context, () => {
+      // DRAW THE OUTER TRIANGLE FRAME
       centerTranslation(context, width, height);
 
-      drawPolygon({ context, ...mask });
+      context.strokeStyle = outerPolyStrokeColor;
+      context.lineWidth = artboard * 0.02;
+      context.globalCompositeOperation = 'color-burn';
+
+      drawPolygon(context, { ...maskOptions, radius: maskOptions.radius - context.lineWidth });
 
       subContext(context, () => {
-        configureShadow({
-          context,
+        // ADD SHADOWN TO OUTER TRIANGLE FRAME
+        configureShadow(context, {
           color: colorUtils.style(deriveLesserColor('black', -10, 0.5)),
           offsetX: 0,
           offsetY: 10,
           blur: 10,
-        })
-
-        context.strokeStyle = outerPolyStrokeColor;
-        context.lineWidth = 30;
+        });
 
         context.stroke();
       });
+
+      // Just for sanity, reset the blend mode.
+      context.globalCompositeOperation = 'source-over';
     });
   };
 };
